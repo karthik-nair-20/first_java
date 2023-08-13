18838f035c68e17835dc78405ebf88f28e82a8dd
kxrthiknair <93530074+kxrthiknair@users.noreply.github.com>
2023-08-13 11:07:43 +0530

    initial commit

---
+++ karthik/greeting.java
@@ -0,0 +1,12 @@
+package com.karthik;
+
+import java.util.Scanner;
+
+public class greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+        System.out.println("Hello "+name+" Welcome to the world of java!!");
+    }
+}
